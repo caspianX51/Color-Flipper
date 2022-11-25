61f2ee127409d17fcbce674cb30b928a17914c13
Oslan VR <alanosmarvira@gmail.com>
2022-11-25 14:01:15 -0600

    Now page changes between four colors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const colors = ["green", "red", "rgba(133, 122, 200)", "f15025"];
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   //get random number between zero and three because colors const has 4 elements. 0,1,2,3.
-  const randomNumber = 2;
+  const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 4);
+}
