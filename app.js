9ea9b44f6ac60adf5126de2a48ab78f59079cef9
Oslan VR <alanosmarvira@gmail.com>
2022-11-25 13:07:50 -0600

    Added style, button changes to purple

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const colors = ["green", "red", "rgba(133, 122, 200)", "f15025"];
 
 const btn = document.getElementById("btn");
-const color = document.querySelector("color");
+const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {});
+btn.addEventListener("click", function () {
+  //get random number between zero and three because colors const has 4 elements. 0,1,2,3.
+  const randomNumber = 2;
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
